a5dbcf61f0e9b7d6b9abca50ba3f5ddfb26cc646
Abiramivannirajan <vabi4135@gmail.com>
2024-10-23 17:55:36 +0530

    upload

---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent {
+
+
+  task=[{taskName:" going to gym", isChecked:false}]
+  
+  constructor(){
+
+  }
+ ngOnInit(): void {
+
+ }
+   onSubmit(form: NgForm){    //template driven forms
+    console.log(form);
+    this.task.push({
+      taskName:form.controls['task'].value,  //two way binding
+      isChecked:false
+    })
+    form.reset()
+   }
+
+   onDelete(index:number){
+     this.task.splice(index,1);
+   }
+
+   onCheck(index:number){
+    console.log(this.task);
+    
+    this.task[index].isChecked=! this.task[index].isChecked;
+   }
+}
